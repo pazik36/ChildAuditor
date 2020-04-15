f996691718da9b2bba5b7359f204f33484fc041a
pazik36 <35067289+pazik36@users.noreply.github.com>
2020-04-15 15:28:20 +0300

    Create Aura.js

---
+++ Aura.js
@@ -0,0 +1,12 @@
+({
+    
+	doInit: function(component, event, helper) {
+		
+        helper.doInit(component, event, helper);
+	},
+    
+    handleClick: function(component, event, helper) {
+        helper.refresh(component, event, helper);
+    },
+    
+})
